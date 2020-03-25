d4fd2a39154c17249a7b0842af8960311d29b013
xaraxx <sara.galvan.o91@gmail.com>
2020-03-25 16:17:09 -0500

    adding flash message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash, get_flashed_messages
+
+import unittest
 
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -54,6 +56,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
+
+        flash('Username succesful registered!')
 
         return redirect(url_for('index'))
 
