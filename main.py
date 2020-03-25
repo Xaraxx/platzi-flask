0925d95897e63486af90dc14fdd65fab493c4926
xaraxx <sara.galvan.o91@gmail.com>
2020-03-24 22:39:24 -0500

    adding username to the context

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -39,14 +39,24 @@ def index():
     return response
 
 
-@app.route('/hello/')
+@app.route('/hello/', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
     context = {
          'user_ip': user_ip, 
          'things_to_do': things_to_do,
-         'login_form': login_form
+         'login_form': login_form,
+         'username': username
     }
     
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
+
+
     return render_template('hello.html', **context)
+
