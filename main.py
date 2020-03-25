de7e2d30bc89c660304ec1dca682c1365c0b8857
xaraxx <sara.galvan.o91@gmail.com>
2020-03-24 20:10:09 -0500

    adding bootstrap, creating forms, adding bootstrap styles to a login form

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask, request, make_response, redirect, render_template
 
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRET'
 
 things_to_do = ['Buy coffee', 'Send requirenment', 'Deliver a video']
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit   = SubmitField('Submit')
 
 @app.errorhandler(500)
 def internal_server(error):
@@ -19,16 +33,19 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello/'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
+    # response.set_cookie('user_ip', user_ip)
     return response
 
 
 @app.route('/hello/')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
          'user_ip': user_ip, 
-         'things_to_do': things_to_do
+         'things_to_do': things_to_do,
+         'login_form': login_form
     }
     
     return render_template('hello.html', **context)
--- starter.sh
+++ starter.sh
@@ -2,6 +2,7 @@
 
 source venv/bin/activate
 
+export FLASK_ENV=development
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 
